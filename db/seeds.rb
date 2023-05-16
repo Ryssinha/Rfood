5ade5297a6a816dab1e5b0f9708488b01d1c7236
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-16 18:30:10 -0300

    feat:first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
+puts "iniciando seed"
 filepath = ENV.fetch('FILEPATH', Rails.root.join('db/states_cities.json').to_s)
 states = JSON.parse(File.read(filepath))
 
 states.each do |state|
-  state_obj = State.find_or_create_by(acronym: state['acronym'], name: state['name'])
+  state_obj = State.find_or_create_by!(acronym: state['acronym'], name: state['name'])
 
   state['cities'].each do |city|
-    City.find_or_create_by(name: city['name'], state: state_obj)
+    City.find_or_create_by!(name: city['name'], state: state_obj)
     Rails.logger.debug { "Adicionando a cidade #{city['name']} ao estado #{state_obj.name}" }
   end
 end
 
-admin_user = User.find_or_create_by(email: 'admin+1@rarolabs.com.br', cpf: '52947611578') do |model|
+admin_user = User.find_or_create_by!(email: 'admin+1@rarolabs.com.br', cpf: '52947611578') do |model|
   model.attributes = {
     name: 'Admin RaroFood',
     password: 'VW1234**'
   }
 end
-Administrator.find_or_create_by(user: admin_user)
+Administrator.find_or_create_by!(user: admin_user)
+
+puts "finalizou seed"
