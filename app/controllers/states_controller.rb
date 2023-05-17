c42fadd7d83f79011b9f75af110c512284afd3e5
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-16 23:36:55 -0300

    feat:009-create_controller_state

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,11 @@
+class StatesController < ApplicationController
+  def index
+    @states = State.all
+    render json: @states
+  end
+
+  def show
+    @state = State.find(params[:id])
+    render json: @state
+  end
+end
