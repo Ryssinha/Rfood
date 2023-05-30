adec3f1c69e88ef891d88abca7251b949b6610b5
Rayssa Guilherme <rayssasoares161@gmail.com>
2023-05-30 16:57:09 -0300

    feat:001-config_sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  # Ideal colocar esses valores como env vars/secrets
+  [user, password] == ['rarofood', 'rarofood@r@r0@c@demy2023']
+  Sidekiq::Extensions.enable_delay!
+
+
+end
